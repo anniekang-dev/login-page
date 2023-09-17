927947d5618124eb4bd7cc2c147eab8da6811e01
anniekang-dev <hyeju0722@gmail.com>
2023-09-17 10:28:58 -0500

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const signIn = document.getElementById('sign-in');
+
+signIn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const user = loginForm.username.value;
+  const pw = loginForm.password.value;
+
+  if (username === "user" && password === )
+})
