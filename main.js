597a520c95ababda49be860d00c82489762dad24
anniekang-dev <hyeju0722@gmail.com>
2023-09-20 18:46:19 -0500

    signup form

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
-// show and hide password
+const pwShowHide = document.querySelectorAll('.fa-regular');
+      pwField = document.querySelectorAll('.password');
+      signUp = document.querySelector('.signup-link');
+      signIn = document.querySelector('.signin-link');
+      loginForm = document.querySelector('.login-form');
+      signupForm = document.querySelector('.signup-form');
+
+// show and hide password / change eye icon
 let eyeIcon = function(icon) {
   icon.classList.toggle("fa-eye-slash");
   icon.classList.toggle("fa-eye");
-
-  let pwShowHide = document.getElementById("user-pw");
-
-  if (pwShowHide.type === "password") {
-      pwShowHide.type = "text";
-  }
-  
-  else {
-    pwShowHide.type = "password";
-  }
 }
+pwShowHide.forEach(eyeIcon => {
+  eyeIcon.addEventListener("click", () => {
+    pwField.forEach(pwField => {
+      if (pwField.type === "password") {
+           pwField.type = "text";
+   }else {
+          pwField.type = "password";
+          }
+    })
+  })
+})
+
+// // change to signup form
+// signUp.addEventListener("click", ()=> {
+//   signupForm.classList.add("active");
+// });
+
+// signIn.addEventListener("click", ()=> {
+//   loginForm.classList.remove("active");
+// });
